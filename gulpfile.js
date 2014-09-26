5243653f37b5a49608bae830f32d70283fb03146
Gundi <goncalo.neto@webdetails.pt>
2014-09-26 16:45:41 +0100

    Clean up package.json and gulpfile.js to do just what is used.

--- gulpfile.js
+++ gulpfile.js
@@ -3,42 +3,9 @@
 
 var gulp = require('gulp'),
     g = require('gulp-load-plugins')({lazy: false}),
-    noop = g.util.noop,
-    bower = require('./bower'),
-    isWatching = false;
+    bower = require('./bower');
 
 var exportPath = './resources/web';
-
-/**
- * Vendors
- */
-gulp.task('vendors', ['vendors-css', 'vendors-js']);
-
-/**
- * Vendors
- */
-gulp.task('vendors-css', function () {
-  return g.bowerFiles()
-    .pipe(g.filter('**/*.css'))
-    .pipe(g.concat('vendors.css'))
-    .pipe(gulp.dest( exportPath + '/css'))
-    .pipe(g.minifyCss())
-    .pipe(g.rename('vendors.min.css'))
-    .pipe(gulp.dest( exportPath + '/css'));
-});
-/**
- * Vendors
- */
-gulp.task('vendors-js', function () {
-  return g.bowerFiles()
-    .pipe(g.filter('**/*.js'))
-    .pipe(g.concat('vendors.js'))
-    .pipe(gulp.dest( exportPath + '/js'))
-    .pipe(g.uglify())
-    .pipe(g.rename('vendors.min.js'))
-    .pipe(gulp.dest( exportPath + '/js'));
-});
-
 
 /**
  * Reveal
